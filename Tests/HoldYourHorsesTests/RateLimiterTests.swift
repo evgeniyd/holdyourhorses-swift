502e3d29238c3f284d826481e3e627b8458cd44c
Yevhen Dubinin <thisiskilling@me.com>
2024-04-10 10:21:59 -0700

    Initial token bucket algorithm with simple test that assumes tokens can be requested

--- Tests/HoldYourHorsesTests/RateLimiterTests.swift
+++ Tests/HoldYourHorsesTests/RateLimiterTests.swift
@@ -10,26 +10,72 @@ protocol HTTPClient {
 
 final class RateLimiter: HTTPClient {
 
-    let client: HTTPClient
+    private let client: HTTPClient
 
-    init(client: HTTPClient) {
+    private var tokens: Int
+    private let maxTokens: Int
+    private var lastRequestTime: Date
+    private let tokenRefreshRate: TimeInterval
+    private let getCurrentDate: () -> Date
+
+    init(client: HTTPClient, maxTokens: Int = 3, tokenRefreshRate: TimeInterval = 1.0, currentDateProvider: @escaping (() -> Date) = Date.init) {
         self.client = client
+        self.maxTokens = maxTokens
+        self.tokenRefreshRate = tokenRefreshRate
+        self.tokens = maxTokens
+        self.getCurrentDate = currentDateProvider
+        self.lastRequestTime = currentDateProvider()
+    }
+
+    private func shouldAllowRequest() -> Bool {
+        refreshTokens()
+
+        guard tokens > 0 else {
+            return false
+        }
+
+        tokens -= 1
+        return true
+    }
+
+    private func refreshTokens() {
+        let now = getCurrentDate()
+        let elapsed = now.timeIntervalSince(lastRequestTime)
+        if elapsed > tokenRefreshRate {
+            let extraTokens = Int(elapsed / tokenRefreshRate) * maxTokens
+            tokens = min(maxTokens, tokens + extraTokens)
+            lastRequestTime = now
+        }
     }
 
     // MARK: HTTPClient
 
     func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
-
+        if shouldAllowRequest() {
+            client.get(from: url, completion: completion)
+        }
     }
 }
 
 final class TestCase: XCTestCase {
 
     func test_init_doesNotRequest() {
-        let client = HTTPClientSpy()
-        let sut = RateLimiter(client: client)
+        let (client, _) = makeSUT()
         XCTAssertTrue(client.requests.isEmpty, "Expects an empty requests list ")
     }
+
+    func test_getFromURL_requestsClientGetFromURL() {
+        var dateProvider = {
+            return Date(timeIntervalSince1970: 0)
+        }
+
+        let (client, sut) = makeSUT(maxTokens: 1, tokenRefreshRate: 1.0, currentDateProvider: dateProvider)
+        sut.get(from: getURL()) { _ in }
+
+        XCTAssertEqual(client.requests.count, 1)
+    }
+
+    // MARK: Helpers
 
     private class HTTPClientSpy: HTTPClient {
         var requests: [URL] = []
@@ -37,5 +83,17 @@ final class TestCase: XCTestCase {
         func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
             requests.append(url)
         }
+    }
+
+    private func makeSUT(maxTokens: Int = 3, tokenRefreshRate: TimeInterval = 1.0, currentDateProvider: @escaping (() -> Date) = Date.init, file: StaticString = #file, line: UInt = #line) -> (client: HTTPClientSpy, sut: RateLimiter) {
+        let client = HTTPClientSpy()
+        let sut = RateLimiter(client: client, maxTokens: maxTokens, tokenRefreshRate: tokenRefreshRate, currentDateProvider: currentDateProvider)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (client, sut)
+    }
+
+    private func getURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
