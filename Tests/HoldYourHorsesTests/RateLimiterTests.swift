063f0d81ab5c5509d60c994c05dbaead64989877
Yevhen Dubinin <thisiskilling@me.com>
2024-04-10 16:53:12 -0700

    Simulate time passing to the next stage

--- Tests/HoldYourHorsesTests/RateLimiterTests.swift
+++ Tests/HoldYourHorsesTests/RateLimiterTests.swift
@@ -126,7 +126,7 @@ final class TestCase: XCTestCase {
     }
 
     func test_getFromURL_failsWithErrorOnceWhenTwoRequestsSentSimultaneously() {
-        let  dateProvider = {
+        let dateProvider = {
             return Date(timeIntervalSince1970: 0)
         }
 
@@ -161,7 +161,7 @@ final class TestCase: XCTestCase {
     }
 
     func test_getFromURL_requestsClientMultipleTimesWhenRequestsSentSimultaneouslyEqualNumberOfTokens() {
-        let  dateProvider = {
+        let dateProvider = {
             return Date(timeIntervalSince1970: 0)
         }
         let (client, sut) = makeSUT(maxTokens: 2, tokenRefreshRate: 1.0, currentDateProvider: dateProvider)
@@ -177,6 +177,44 @@ final class TestCase: XCTestCase {
             }
             exp1.fulfill()
         }
+
+        sut.get(from: getURL()) { result in
+            switch result {
+                case .success:
+                    receivedResponsesCount += 1
+                case .failure: break
+            }
+            exp2.fulfill()
+        }
+
+        client.complete(at:0)
+        client.complete(at:1)
+
+        wait(for: [exp1, exp2], timeout: 0.1)
+
+        XCTAssertEqual(receivedResponsesCount, 2)
+    }
+
+    func test_getFromURL_requestsClientMultipleTimesWhenRequestsSentAtTokenRefreshRate() {
+        var timeBox = Box<TimeInterval>(0)
+        let  dateProvider = { [timeBox] in
+            return Date(timeIntervalSince1970: timeBox.value)
+        }
+        let (client, sut) = makeSUT(maxTokens: 1, tokenRefreshRate: 1.0, currentDateProvider: dateProvider)
+        let exp1 = expectation(description: "Did received result")
+        let exp2 = expectation(description: "Did received result")
+        var receivedResponsesCount = 0
+
+        sut.get(from: getURL()) { result in
+            switch result {
+                case .success:
+                    receivedResponsesCount += 1
+                case .failure: break
+            }
+            exp1.fulfill()
+        }
+
+        timeBox.value += 2.0
 
         sut.get(from: getURL()) { result in
             switch result {
@@ -216,6 +254,13 @@ final class TestCase: XCTestCase {
 
         func complete(with error: Error, at index: Int = 0) {
 
+        }
+    }
+
+    private final class Box<T> {
+        var value: T
+        init(_ value: T) {
+            self.value = value
         }
     }
 
